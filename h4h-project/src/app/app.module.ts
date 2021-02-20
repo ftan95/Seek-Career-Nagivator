f5b891db8bdb48b9199ea6d4d65e3719639dc518
Frederick Tan <ftan@fredericks-mbp.attlocal.net>
2021-02-20 13:20:35 -0800

    Added background colour

--- h4h-project/src/app/app.module.ts
+++ h4h-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {Routes, RouterModule, Router} from '@angular/router';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -36,6 +37,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
--- h4h-project/src/app/home/home.component.ts
+++ h4h-project/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
@@ -10,10 +11,22 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private readonly router: Router,
-    private readonly route: ActivatedRoute
-  ) { }
+    private readonly route: ActivatedRoute,
+    private readonly fb: FormBuilder
+  ) { 
+    this.loginForm = this.fb.group({
+      user: '',
+      password: ''
+    })
+  }
+
+  loginForm: FormGroup;
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'user': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required)
+    });
   }
 
 }
