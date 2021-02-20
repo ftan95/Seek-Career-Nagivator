41864fedcd08033c18dc16223f9962ae9e894be8
Frederick Tan <ftan@fredericks-mbp.attlocal.net>
2021-02-20 12:38:45 -0800

    Added routing and page 2

--- h4h-project/src/app/app.module.ts
+++ h4h-project/src/app/app.module.ts
@@ -1,15 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes, RouterModule, Router} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OptionsComponent } from './options/options.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: 'Home Page'
+    }
+  },
+  {
+    path: 'option',
+    component: OptionsComponent,
+    data: {
+      title: 'Options'
+    }
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OptionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
